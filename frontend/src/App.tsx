542c4fb4179a082a92ba1aceef85ad84efbec9cb
Ingrid Nogueira <isnogueira601@gmail.com>
2021-09-07 11:52:33 -0300

    Bootstrapp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div>
-      <h1>Olá mundo!</h1>
+    <div className="container text-center">
+      <h1 className="text-primary">Olá mundo!</h1>
     </div>
   );
 }
