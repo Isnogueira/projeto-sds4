6c7748b7ef52443d4fe57448e15bb13edbe654f8
Ingrid Nogueira <isnogueira601@gmail.com>
2021-09-07 10:32:54 -0300

    Project created

---
+++ backend/src/test/java/br/edu/infnet/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
