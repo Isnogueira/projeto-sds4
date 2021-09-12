852fc0280cd3d00f2de78877b3f61f72c3f32cc2
Ingrid Nogueira <isnogueira601@gmail.com>
2021-09-12 13:45:38 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,12 +1,35 @@
+import axios from "axios";
 import Chart from "react-apexcharts";
+import {BASE_URL} from "utils/requests";
+import {SaleSum} from "types/sale";
 
+type ChatData = {
+    labels : string[]
+    series : number[]
+}
 
 function DonutChart() {
 
-    const mockData = {
+    // FORMA ERRADA
+    let chatData : ChatData = { labels: [], series: []};
+
+    // FORMA ERRADA
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response=> {
+
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            let chatData : ChatData = { labels: myLabels, series: mySeries};
+
+            console.log(chatData);
+        })
+
+  /*  const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    }*/
     
     const options = {
         legend: {
@@ -18,8 +41,8 @@ function DonutChart() {
     return (
 
         <Chart
-            options={{...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{...options, labels: chatData.labels}}
+            series={chatData.series}
             type="donut"
             height="240"
 
